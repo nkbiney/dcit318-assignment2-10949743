34cbb6e49b62cd24e1d1f0e522af2b93206bdabe
nkbiney <nkabiney001@st.ug.edu.gh>
2024-06-16 22:27:53 +0100

    First Commit

---
+++ AnimalSounds/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MovableExample
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    public class MovExample
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Move();
+
+            Bicycle bicycle = new Bicycle();
+            bicycle.Move();
+        }
+    }
+}
